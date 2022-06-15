87b2a85b588cbc23fd5e9195ac906a71d6aa00a3
Nitesh <91332849+hemlo69@users.noreply.github.com>
2022-06-15 21:36:43 +0530

    Update lineage_RMX2020.mk

--- lineage_RMX2020.mk
+++ lineage_RMX2020.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_RMX2020
